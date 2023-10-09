c0f3feab8b26e3c035345cefbad21748fc528678
谢先生 <8008121104@email.ncu.edu.cn>
2023-10-09 16:22:46 +0800

    login

--- src/main/java/com/hmdp/config/MvcConfiguration.java
+++ src/main/java/com/hmdp/config/MvcConfiguration.java
@@ -1,7 +1,7 @@
 package com.hmdp.config;
 
-import com.hmdp.utils.LoginInterceptor;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.hmdp.interceptor.LoginInterceptor;
+import com.hmdp.interceptor.RefreshTokenInterceptor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
@@ -17,11 +17,15 @@ public class MvcConfiguration implements WebMvcConfigurer {
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(new LoginInterceptor(stringRedisTemplate))
+
+        registry.addInterceptor(new LoginInterceptor())
                 .excludePathPatterns(
                         "/shop/**",
                         "/user/code",
                         "/user/login"
-                        );
+                        ).order(1);
+
+        registry.addInterceptor(new RefreshTokenInterceptor(stringRedisTemplate)).order(0);
+
     }
 }
